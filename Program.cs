a91da0c6a2ae848ee64e13daf8209ad36a5506d5
TimTieng <timothy.tieng@gmail.com>
2021-04-13 12:03:27 -0400

    problem complete, shifting to refactor

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace NonRepeatingCharacters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Test1 -- ExpOut = 1
+            string string1 = "abcdcaf";
+            FirstNonRepeatingCharacter(string1);
+
+            // Test2
+            string string2 = "faadabcbbebdf";
+            FirstNonRepeatingCharacter(string2);
+        }
+        public static void FirstNonRepeatingCharacter(string str)
+        {
+            // Declare pointers and loop condition
+            int nonRepeatIndex = 0;
+            int traverser = 0;
+            bool foundDuplicate = false;
+
+            // Type conversion required to iterate
+            char[] charArr = str.ToCharArray();
+
+            // While Iteration - Loop while nonRepeatIndex < overall length
+            while(nonRepeatIndex < charArr.Length)
+            {
+                // Assume no duplicates found when entering the general loop
+                foundDuplicate = false;
+                // Iterative Evaluation
+                for (traverser =0; traverser < charArr.Length; traverser++)
+                {
+                    if (charArr[nonRepeatIndex] == charArr[traverser] && nonRepeatIndex != traverser)
+                    {
+                        foundDuplicate = true;
+                    }
+                }
+                if (!foundDuplicate)
+                {
+                    Console.WriteLine($"The index of non-repeating character is: {nonRepeatIndex}");
+                }
+                // Increment nonRepeatIndex to continue loop
+                nonRepeatIndex++;
+            }
+            Console.WriteLine("There are no repeating characters in your array");
+        }
+    }
+}
